a26f3e95e175f36758b5fb919739a2ec6455caed
Elisa Pheline Weber <elisa.pheline.weber@web.de>
2021-02-07 21:24:48 +0100

    removed unnecessary months

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+months = ['january', 'february', 'march', 'april', 'may', 'june', 'july']
 
 days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 
